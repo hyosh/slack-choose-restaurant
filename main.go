d7613efcc9f12146f1457989c140bb7e70e3b965
François <francois@oniti.fr>
2023-02-24 09:47:17 +0100

    correction

--- main.go
+++ main.go
@@ -6,11 +6,19 @@ import (
 	"time"
 )
 
+const Version = "1.0.0"
+
 func main() {
 	port := flag.Int("p", 8080, "port to listen on")
 	host := flag.String("h", "", "host to listen on")
+	versionFlag := flag.Bool("v", false, "print version and exit")
 
 	flag.Parse()
+
+	if *versionFlag {
+		println("Version:", Version)
+		return
+	}
 
 	// init random
 	rand.Seed(time.Now().UnixNano())
